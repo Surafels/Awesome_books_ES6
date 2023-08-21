d30d6a0048730a2e2ada4a57963eb4534223d23b
Surafels <ssurafel4@gmail.com>
2023-08-21 22:03:44 +0300

    Add index.html

---
+++ modules/variable.js
@@ -0,0 +1,16 @@
+const bookTitle = document.getElementById('book-title');
+const bookAuthor = document.getElementById('book-author');
+const addBtn = document.getElementById('btn');
+const listEntry = document.getElementById('list-entry');
+const listLink = document.querySelector('#bk-list');
+const addLink = document.querySelector('#bk-add');
+const listContact = document.querySelector('#bk-contact');
+const sectionList = document.querySelector('#list');
+const sectionNew = document.querySelector('#new');
+const sectionContact = document.querySelector('#contact');
+const showDate = document.querySelector('.show-date');
+
+export { bookTitle, bookAuthor,addBtn, listEntry , listLink ,addLink , listContact ,sectionList ,sectionNew ,sectionContact ,showDate
+
+};
+
